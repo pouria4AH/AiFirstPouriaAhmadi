157a5e49d69cfccc237d488a1b1e25404713ae8d
net developer <4pouria.ahmadi.2oo1@gmail.com>
2022-04-29 20:51:01 +0430

    Add project files.

---
+++ AiFirstPouriaAhmadi/Program.cs
@@ -0,0 +1,31 @@
+﻿using AiFirstPouriaAhmadi;
+
+logic newLogic = new logic();
+CarState carState = CarState.Start;
+bool key = true;
+while (key)
+{
+    Console.WriteLine("chose car state \n\n");
+    Console.WriteLine("S for so close \n");
+    Console.WriteLine("C for close \n");
+    Console.WriteLine("F for Far \n");
+    Console.WriteLine("N so NotExist \n");
+    string res = Console.ReadLine();
+
+    switch (res.ToLower())
+    {
+        case "s":
+            carState = CarState.SoClose;
+            break;
+        case "c":
+            carState = CarState.Close;
+            break;
+        case "f":
+            carState = CarState.Far;
+            break;
+        case "n":
+            carState = CarState.NotExist;
+            break;
+    }
+    key = newLogic.Walking(carState);
+}
---
+++ AiFirstPouriaAhmadi/logic.cs
@@ -0,0 +1,69 @@
+﻿namespace AiFirstPouriaAhmadi
+{
+    public class logic
+    {
+        public CarState CarState { get; set; }
+        public int PathForWalking { get; set; } = 0;
+
+        public bool Walking(CarState state)
+        {
+            if (PathForWalking >= 10)
+            {
+                Console.WriteLine("you are in end place ");
+                return false;
+            }
+            switch (state)
+            {
+                case CarState.NotExist:
+                    PathForWalking += 2;
+                    if (PathForWalking > 10) PathForWalking = 10;
+                    Console.WriteLine($"two step next you are have {10 - PathForWalking} step");
+                    break;
+                case CarState.Far:
+                    PathForWalking += 1;
+                    if (PathForWalking > 10) PathForWalking = 10;
+                    Console.WriteLine($"one step next you are have {10 - PathForWalking} step");
+                    break;
+                case CarState.Close:
+                    if (PathForWalking < 0)
+                        Console.WriteLine("You are in start place");
+                    else
+                    {
+                        PathForWalking -= 1;
+                        if (PathForWalking < 0) PathForWalking = 0;
+                        Console.WriteLine($"one step back you are have {10 - PathForWalking} step");
+                    }
+                    break;
+                case CarState.SoClose:
+                    if (PathForWalking < 0)
+                        Console.WriteLine("You are in start place");
+                    else
+                    {
+                        PathForWalking -= 2;
+                        if (PathForWalking < 0) PathForWalking = 0;
+                        Console.WriteLine($"two step back you are have {10 - PathForWalking} step");
+                    }
+                    break;
+            }
+
+            if (PathForWalking >= 10)
+            {
+                Console.WriteLine("you are in end place ");
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+
+    public enum CarState
+    {
+        Start,
+        Close,
+        SoClose,
+        Far,
+        NotExist
+
+    }
+}
